86a160b49f7a6da2cc4b5b8cb9a0c77a082c5248
60200124152-spec <60200124152@uin-alauddin.ac.id>
2026-05-04 18:32:56 +0800

    menambah fitur create task

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TaskController; // Tambahkan ini
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Taruh di sini
 Route::post('/products', [ProductController::class, 'store']);
+
+// Taruh route untuk To-Do List di sini
+Route::post('/tasks', [TaskController::class, 'store']);
