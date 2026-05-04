39083235c06ed4555f32b7f744616264985654ba
60200124152-spec <60200124152@uin-alauddin.ac.id>
2026-05-04 18:36:22 +0800

    menyelesaikan fitur CRUD to-do list lengkap

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,14 +7,34 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    // Fitur READ [GET] - Menampilkan semua tugas
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    // Fitur CREATE [POST] - Menambah tugas baru
     public function store(Request $request)
     {
         $validated = $request->validate([
             'title' => 'required|string|max:255',
         ]);
 
-        Task::create($validated);
+        $task = Task::create($validated);
 
-        return response()->json(['message' => 'Task berhasil ditambah!'], 201);
+        return response()->json([
+            'message' => 'Task berhasil ditambah!',
+            'data' => $task
+        ], 201);
+    }
+
+    // Fitur DELETE [DELETE] - Menghapus tugas
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return response()->json(['message' => 'Task berhasil dihapus!']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\TaskController; // Tambahkan ini
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,5 +10,6 @@ Route::get('/', function () {
 
 Route::post('/products', [ProductController::class, 'store']);
 
-// Taruh route untuk To-Do List di sini
-Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks', [TaskController::class, 'index']);      
+Route::post('/tasks', [TaskController::class, 'store']);     
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']); 
